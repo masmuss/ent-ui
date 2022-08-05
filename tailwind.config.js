f0984e59e4db2e1e0dd21438e327dd190e3f04ef
A. Musafir Khoirul F <ahmusafir@it.student.pens.ac.id>
2022-08-05 22:10:12 +0700

    add about us and teams products section

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,22 +1,32 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["public/**/*.{html, js}"],
-  theme: {
-    extend: {},
-    container: {
-      center: true,
-      padding: {
-        default: "1rem",
-        sm: "1rem",
-        md: "1.5rem",
-        lg: "2rem",
-        xl: "3rem",
-        "2xl": "4rem",
-      },
-    },
-    fontFamily: {
-      sans: ["Inter", "sans-serif"],
-    },
-  },
-  plugins: [require("prettier-plugin-tailwindcss")],
+	content: ["public/**/*.{html, js}"],
+	theme: {
+		extend: {},
+		colors: {
+			/**
+			 * example:
+			 * 'blue': '#1fb6ff',
+			 */
+			// primary
+			// secondary
+			// accent
+			// background
+		},
+		container: {
+			center: true,
+			padding: {
+				default: "1rem",
+				sm: "1rem",
+				md: "1.5rem",
+				lg: "2rem",
+				xl: "3rem",
+				"2xl": "4rem",
+			},
+		},
+		fontFamily: {
+			sans: ["Inter", "sans-serif"],
+		},
+	},
+	plugins: [require("prettier-plugin-tailwindcss")],
 };
