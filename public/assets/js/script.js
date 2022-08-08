20b20f77ed74b74f694c797404445485d1ec1286
A. Musafir Khoirul F <ahmusafir@it.student.pens.ac.id>
2022-08-08 18:16:37 +0700

    add FAQ section

--- public/assets/js/script.js
+++ public/assets/js/script.js
@@ -37,3 +37,28 @@ window.onclick = function (event) {
 		}
 	}
 };
+
+// accordion
+const accordionHeader = document.querySelectorAll(".accordion-header");
+accordionHeader.forEach((header) => {
+	header.addEventListener("click", function () {
+		const accordionContent =
+			header.parentElement.querySelector(".accordion-content");
+		let accordionMaxHeight = accordionContent.style.maxHeight;
+
+		// Condition handling
+		if (accordionMaxHeight == "0px" || accordionMaxHeight.length == 0) {
+			accordionContent.style.maxHeight = `${
+				accordionContent.scrollHeight + 32
+			}px`;
+			header.parentElement.classList.add("bg-gray-100");
+			header.querySelector(".fas").classList.remove("fa-plus");
+			header.querySelector(".fas").classList.add("fa-minus");
+		} else {
+			accordionContent.style.maxHeight = `0px`;
+			header.parentElement.classList.remove("bg-gray-100");
+			header.querySelector(".fas").classList.add("fa-plus");
+			header.querySelector(".fas").classList.remove("fa-minus");
+		}
+	});
+});
