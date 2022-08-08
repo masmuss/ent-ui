c7e7fcf0a3f8cf000c231774efc81ac654a066d3
A. Musafir Khoirul F <ahmusafir@it.student.pens.ac.id>
2022-08-08 12:23:58 +0700

    fix responsive design on any section

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,21 +2,24 @@
 module.exports = {
 	content: ["public/**/*.{html, js}"],
 	theme: {
-		extend: {},
-		  
-		container: {
-			center: true,
-			padding: {
-				default: "1rem",
-				sm: "1rem",
-				md: "1.5rem",
-				lg: "2rem",
-				xl: "3rem",
-				"2xl": "4rem",
+		extend: {
+			aspectRatio: {
+				"4/3": "4 / 3",
 			},
-		},
-		fontFamily: {
-			sans: ["Inter", "sans-serif"],
+			container: {
+				center: true,
+				padding: {
+					default: "1rem",
+					sm: "1.5rem",
+					md: "1.5rem",
+					lg: "2rem",
+					xl: "3rem",
+					"2xl": "4rem",
+				},
+			},
+			fontFamily: {
+				sans: ["Inter", "sans-serif"],
+			},
 		},
 	},
 	plugins: [require("prettier-plugin-tailwindcss")],
